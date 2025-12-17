61086a54903e1efdd56875ad35147a41d8ad0e24
Kidundone <134435473+Kidundone@users.noreply.github.com>
2025-12-16 21:08:29 -0500

    Fix GitHub Pages PWA home-screen 404 (scope/start_url/relative paths)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "flat-rate-log-v11";
+const CACHE = "flat-rate-log-v12";
 const ASSETS = ["./index.html","./app.js","./manifest.webmanifest","./sw.js"];
 
 self.addEventListener("install", (event) => {
