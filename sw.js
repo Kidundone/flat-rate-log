88dd38487dfefbcfbb40290598c86170e38c0118
Kidundone <134435473+Kidundone@users.noreply.github.com>
2026-01-29 10:07:10 -0500

    fix: force SW update

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const SW_VERSION = "2026-01-29-14-30-35";
-const CACHE = "frlog-20260107a";
+const SW_VERSION = "2026-01-29-15-07-10";
+const CACHE_NAME = `fr-cache-${SW_VERSION}`;
+
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
 
 self.addEventListener("install", (e) => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll([
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll([
     "./",
     "./index.html",
     "./more.html",
@@ -17,8 +19,7 @@ self.addEventListener("install", (e) => {
 self.addEventListener("activate", (e) => {
   e.waitUntil((async () => {
     const keys = await caches.keys();
-    await Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
-    await self.clients.claim();
+    await Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
   })());
 });
 
